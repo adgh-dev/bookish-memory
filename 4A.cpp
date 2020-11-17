ac888da00eaed71dba41b316c96ec7e301f86958
Andrei Gheorghiu <andreiebh@gmail.com>
2020-11-17 13:34:15 +0200

    initial commit

---
+++ 4A.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+string watermelon_divs_even(int weight) {
+    if (weight % 2 == 0) {
+        weight -= 2;
+        if (weight % 2 == 0) {
+            return "YES";
+        }
+    }
+    return "NO";
+}
+
+int main() {
+    int w;
+    cin >> w;
+    cout << watermelon_divs_even(w) << endl;
+    return 0;
+}
